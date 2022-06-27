326d0a5b5e69ba233084ed4505091cbf536151e1
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-06-27 15:24:26 -0700

    made display for 10 gifs
    Co-Authored-By: Filmer Tolentino <filmer.tolentino@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,3 +2,35 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+$(document).ready(function() {
+  $('#go').click(function(event) {
+    event.preventDefault();
+    console.log("hello");
+    const search = $('#search').val();
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${search}&limit=10`;
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200){
+        var response = JSON.parse(this.responseText);
+        console.log(response);
+        robustShowImage(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+    function showImage(response) {
+      $('.output').html(`<img src="${response.data[2].images.original.url}" />`);
+    }
+
+    function robustShowImage(response) {
+      let outputString = "";
+      response.data.forEach(element => outputString += `<img src="${element.images.original.url}" />`);
+      $('.output').html(outputString);
+    }
+  });
+});
+
