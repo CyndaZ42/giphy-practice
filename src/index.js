7579b8d02b8506f5799b63f4d76209c5a8128346
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-06-27 15:50:50 -0700

    add trending and random buttons
    Co-Authored-By: Filmer Tolentino <filmer.tolentino@gmail.com>

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     const search = $('#search').val();
 
     let request = new XMLHttpRequest();
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${search}&limit=10`;
+    const url = `http://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${search}&limit=20`;
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200){
         var response = JSON.parse(this.responseText);
@@ -21,16 +21,45 @@ $(document).ready(function() {
 
     request.open("GET", url, true);
     request.send();
+  });
+  $('#popular').click(function(event) {
+    event.preventDefault();
+    let request = new XMLHttpRequest();
+    const trending = `http://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=10`;
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200){
+        var response = JSON.parse(this.responseText);
+        console.log(response);
+        robustShowImage(response);
+      }
+    }
+    request.open("GET", trending, true);
+    request.send();
+  });
+
+  $('#random').click(function(event) {
+    event.preventDefault();
+    let request = new XMLHttpRequest();
+    const random = `http://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}`;
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200){
+        var response = JSON.parse(this.responseText);
+        console.log(response);
+        showImage(response);
+      }
+    }
+    request.open("GET", random, true);
+    request.send();
 
     function showImage(response) {
-      $('.output').html(`<img src="${response.data[2].images.original.url}" />`);
-    }
-
-    function robustShowImage(response) {
-      let outputString = "";
-      response.data.forEach(element => outputString += `<img src="${element.images.original.url}" />`);
-      $('.output').html(outputString);
+      $('.output').html(`<img src="${response.data.images.original.url}" />`);
     }
   });
+
+    function robustShowImage(response) {
+    let outputString = "";
+    response.data.forEach(element => outputString += `<img src="${element.images.downsized_medium.url}" />`);
+    $('.output').html(outputString);
+  }
 });
 
